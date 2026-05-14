f998c62102029af3f42ad20d58adb3db9f157a5b
Jose <alfredo.mori1999@gmail.com>
2026-05-14 15:32:14 -0500

    sdadasdaasda

---
+++ src/main/java/com/example/codigo/Usuario/Usuario.java
@@ -0,0 +1,14 @@
+package com.example.codigo.Usuario;
+
+public class Usuario {
+
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ src/main/java/com/example/codigo/lambda/Operacion.java
@@ -0,0 +1,8 @@
+package com.example.codigo.lambda;
+
+@FunctionalInterface
+public interface Operacion {
+
+    int calcular (int a , int b);
+
+}
